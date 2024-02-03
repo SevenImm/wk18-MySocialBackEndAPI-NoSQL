2e5f2eeb560f6b5b276ca2835c1b425ae1dd2b0d
SevenImm <angelcardenasptx@gmail.com>
2024-02-03 13:45:36 -0600

    Adding folders 01

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
 const express = require('express');
-// initialize MongoDBCLient class
-const { MongoClient } = require('mongodb');
+const db = require('./config/connection');
+const routes = require('./routes');
 
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-const port = 3001;
 
-// Connection string to local instance of MongoDB
-const connectionStringURI = `mongodb:localhost:27017`;
+// // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+// const activity = cwd.includes('01-Activities')
+//   ? cwd.split('01-Activities')[1]
+//   : cwd;
 
-// Initialize a new instance of MongoClient;
-const client = new MongoClient(connectionStringURI);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-// Declare a variable to hold the connection
-let myDB;
-
-// Create variable to hold our database name
-const dbName = 'mySocialAPI';
-
-// Use Connect method to connect to the mongo server
-client.connect()
-.then(() => {
-    console.log('Connected successfully to MongoDB');
-    // Use client.db() constructor to add a new db instance
-    db = client.db(dbName);
-
-    // Start up express server
-    app.listen(port, () => {
-        console.log (`Example app listening at http://localhost:${port}`);
-    });
-})
-.catch((err) => {
-    console.error('Mongo connection error: ', err.message);
-})
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
